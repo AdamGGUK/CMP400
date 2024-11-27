c47bc9c306d070a0e337f35a9caff36322097ed0
AdamGGUK <78075579+AdamGGUK@users.noreply.github.com>
2024-11-27 02:25:40 +0000

    Feasibility Demo
    Added basic functionality.

--- Assets/AIDirector.cs
+++ Assets/AIDirector.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AIDirector : MonoBehaviour
 {
@@ -17,7 +18,15 @@ public class AIDirector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
         
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 
     public List<CopScript> getCops()
